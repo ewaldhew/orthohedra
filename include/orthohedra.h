39d922f641d3d8e09508bb7ec79abcbcb0dc84e0
Ewald Hew <ewaldhew@gmail.com>
2018-08-07 13:38:56 +0800

    Whitespace.

--- include/orthohedra.h
+++ include/orthohedra.h
@@ -6,21 +6,21 @@ extern "C"
 {
 #endif
 
-  typedef struct {
+typedef struct {
     void *repr;
-  } OPP;
+} OPP;
 
-  /* Initializes an orthogonal pseudo-polyhedron */
-  OPP* OH_New();
+/* Initializes an orthogonal pseudo-polyhedron */
+OPP* OH_New();
 
-  void OH_Destroy(OPP* o);
+void OH_Destroy(OPP* o);
 
-  OPP* OH_Complement(OPP* o);
-  OPP* OH_Intersection(OPP* o1, OPP* o2);
-  OPP* OH_Union(OPP* o1, OPP* o2);
-  OPP* OH_Difference(OPP* o1, OPP* o2);
+OPP* OH_Complement(OPP* o);
+OPP* OH_Intersection(OPP* o1, OPP* o2);
+OPP* OH_Union(OPP* o1, OPP* o2);
+OPP* OH_Difference(OPP* o1, OPP* o2);
 
-  int OH_Output_Repr(OPP* o, char** buffer, int* size);
+int OH_Output_Repr(OPP* o, char** buffer, int* size);
 
 #ifdef __cplusplus
 }
